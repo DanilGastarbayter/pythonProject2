4bd4604caf940ba222db799d0d0513b54b442982
DanilGastarbayter <h2oo2h22@gmail.com>
2022-11-12 13:17:08 +0200

    сомміт

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+try:
+    print('start code')
+    print('no errors')
+except NameError:
+    print("We have an error nameerror ")
+except ZeroDivisionError:
+    print("We have an error zerodivisionerror ")
+print('The end')
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+try:
+    print('start code')
+    print(5/0)
+    print('no errors')
+except (NameError, ZeroDivisionError) as error:
+    print("error")
+print('The end')
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 try:
     try:
         print('start code')
-        print(error)  # помилка
+        print(error)
         print('no error')
     except SyntaxError:
         print("Wrong Syntax")
-except
+except NameError as error:
+    print(error)
